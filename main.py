1243dd8075ac0645159bc1a2b8f2d821604ab5e9
Katie Wu <554855799@qq.com>
2026-01-31 22:50:45 +0800

    add shot

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
+import sys
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -13,9 +15,11 @@ def main():
     asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     AsteroidField()
 
@@ -38,6 +42,13 @@ def main():
         # for updatable_object in updatable:
         #     updatable_object.update(dt)
         updatable.update(dt)
+        for ansteroid in asteroids:
+            if ansteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+                # pygame.quit()
+                # exit()
         # print(dt)
         # print(f"FPS: {clock.get_fps()}")
     # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
